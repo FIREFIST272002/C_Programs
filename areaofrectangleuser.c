94aa8a555494a196a0163754e59e61e45be0a111
FIREFIST272002 <121559753+FIREFIST272002@users.noreply.github.com>
2024-05-06 00:54:25 +0530

    Add files via upload

---
+++ areaofrectangleuser.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    float length;
+    float breadth;
+    printf("The length of the rectangle is\n");
+    scanf("%f",&length);
+
+    printf("The breadth of the rectangle is\n");
+    scanf("%f",&breadth);
+    printf("The area of the rectangle is %f\n",length*breadth);
+
+
+
+    return 0;
+
+}
