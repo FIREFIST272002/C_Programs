a89dde163a0f03d5dcc89002edcfe5f37a12ca2a
FIREFIST272002 <121559753+FIREFIST272002@users.noreply.github.com>
2024-05-06 01:31:51 +0530

    Add files via upload

---
+++ areaofcircle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    float pi=3.14;
+    float radius;
+    float height;
+    printf("The radius of the circle is\n");
+    scanf("%f",&radius);
+    printf("The height of the cylinder is \n");
+    scanf("%f",&height);
+    printf("The area of circle is %f\n",pi*radius*radius);
+    printf("The area of cylinder is %f\n",2*pi*radius*height+2*pi*radius*radius);
+
+    return 0;    
+}
