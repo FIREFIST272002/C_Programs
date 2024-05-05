36abb84b90ce44f28e2c0fa061b47569401c2a08
FIREFIST272002 <121559753+FIREFIST272002@users.noreply.github.com>
2024-05-06 00:54:02 +0530

    Add files via upload

---
+++ userinputaddition.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("The value of a is\n");
+    scanf("%d",&a);
+    printf("The value of b is\n");
+    scanf("%d",&b);
+    printf("The value of a+b is %d\n",a+b);
+
+
+    return 0;
+}
