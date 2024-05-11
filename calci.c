3e02a1e8d5d71902471d96b391fe926e30b03017
FIREFIST272002 <121559753+FIREFIST272002@users.noreply.github.com>
2024-05-11 19:53:30 +0530

    Add files via upload

---
+++ calci.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a=4;
+    int b=8;
+    printf("Addition %d \n",a+b);
+    printf("Subtraction %d \n",a-b);
+    printf("Division %d \n",a/b);
+    printf("Multiplication %d \n",a*b);
+
+
+
+
+    return 0;
+
+}
